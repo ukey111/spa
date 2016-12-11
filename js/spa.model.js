c50954dd94085c8910c5a022276de2b315148c29
UKey111 <youkey111@gmail.com>
2016-12-12 00:38:09 +0900

    メッセージング追加

--- js/spa.model.js
+++ js/spa.model.js
@@ -58,6 +58,8 @@ spa.model = (function (){
         stateMap.user.css_map = user_map.css_map;
         stateMap.people_cid_map[ user_map._id ] = stateMap.user;
 
+        chat.join();
+
         // チャットを追加するときには、ここで参加すべき
         $.gevent.publish( 'spa-login', [ stateMap.user ] );
     };
@@ -133,6 +135,7 @@ spa.model = (function (){
             var is_removed, user = stateMap.user;
             // チャットを追加するときには、ここでチャットルームから出るべき
 
+            chat._leave();
             is_removed    = removePerson( user );
             stateMap.user = stateMap.anon_user;
 
@@ -155,13 +158,17 @@ spa.model = (function (){
     // chat object API
     chat = (function () {
         var
-            _publish_listchange,
-            _update_list, _leave_chat, join_chat;
+            _publish_listchange, _publish_updatechat,
+            _update_list, _leave_chat, join_chat,
+
+            get_chatee, join_chat, send_msg, set_chatee,
+            chatee = null;
 
         // 内部メソッド開始
         _update_list = function( arg_list ) {
             var i, person_map, make_person_map,
-                people_list = arg_list[ 0 ];
+                people_list = arg_list[ 0 ],
+                is_chatee_online = false;
 
             clearPeopleDb();
 
@@ -186,24 +193,51 @@ spa.model = (function (){
                     name   : person_map.name
                 };
 
+                if ( chatee && chatee.id === make_person_map.id ) {
+                    is_chatee_online = true;
+                }
+
                 makePerson( make_person_map );
             }
             stateMap.people_db.sort( 'name' );
+
+            // チャット相手がオンラインでなくなっている場合は、チャット相手を解除する。
+            // その結果、「spa-setchatee」グローバルイベントが発行される。
+            if ( chatee && ! is_chatee_online ) {
+                set_chatee('');
+            }
         };
 
         _publish_listchange = function ( arg_list ) {
             _update_list( arg_list );
             $.gevent.publish( 'spa-listchange', [ arg_list ] );
         };
+
+        _publish_updatechat = function ( arg_list ) {
+            var msg_map = arg_list[ 0 ];
+
+            if ( ! chatee ) {
+                set_chatee( msg_map.sender_id );
+            }
+            else if ( msg_map.sender_id !== stateMap.user.id && msg_map.sender_id !== chatee.id ) {
+                set_chatee( msg_map.sender_id );
+            }
+            $.gevent.publish( 'spa-updatechat', [ msg_map ] );
+        };
         // 内部メソッド終了
 
         _leave_chat = function ()   {
             var sio = isFakeData ? spa.fake.mockSio : spa.data.getSio();
+            chatee = null;
             stateMap.is_connected = false;
             if ( sio ) {
                 sio.emit( 'leavechat' );
             }
         };
+
+        get_chatee = function () {
+            return chatee;
+        }
 
         join_chat = function () {
             var sio;
@@ -219,13 +253,61 @@ spa.model = (function (){
 
             sio = isFakeData ? spa.fake.mockSio : spa.data.getSio();
             sio.on( 'listchange', _publish_listchange );
+            sio.on( 'updatechat', _publish_updatechat );
             stateMap.is_connected = true;
             return true;
         };
 
+        send_msg = function ( msg_text ) {
+            var msg_map,
+                sio = isFakeData ? spa.fake.mockSio : spa.data.getSio();
+
+            if ( ! sio ) {
+                return false;
+            }
+
+            if ( ! ( stateMap.user && chatee ) ) {
+                return false;
+            }
+
+            msg_map = {
+                dest_id  : chatee.id,
+                dest_name: chatee.name,
+                sender_id: stateMap.user.id,
+                msg_text : msg_text
+            };
+
+            // updatechatを発行したので、送信メッセージを表示できる
+            _publish_updatechat( [ msg_map ] );
+            sio.emit( 'updatechat', msg_map );
+            return true;
+        };
+
+        set_chatee = function ( person_id ) {
+            var new_chatee;
+            new_chatee = stateMap.people_cid_map[ person_id ];
+            if ( new_chatee ) {
+                if ( chatee && chatee.id === new_chatee.id ) {
+                    return false;
+                }
+            }
+            else {
+                new_chatee = null;
+            }
+
+            $.gevent.publish( 'spa-setchatee',
+                { old_chatee : chatee, new_chatee : new_chatee }
+            );
+            chatee = new_chatee;
+            return true;
+        };
+
         return {
-            _leave: _leave_chat,
-            join  : join_chat
+            _leave    : _leave_chat,
+            get_chatee: get_chatee,
+            join      : join_chat,
+            send_msg  : send_msg,
+            set_chatee: set_chatee
         };
     }());
 
