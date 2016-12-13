9150a69bffd649a3fcbc4b496aa010f512704ee2
UKey111 <youkey111@gmail.com>
2016-12-14 03:01:12 +0900

    アバターサポート追加

--- js/spa.fake.js
+++ js/spa.fake.js
@@ -53,7 +53,7 @@ spa.fake = (function () {
       };
 
       emit_sio = function ( msg_type, data ) {
-        var person_map;
+        var person_map, i;
 
         // 3秒間の遅延後に「userupdate」コールバックで
         // 「adduser」イベントに応答する
@@ -100,6 +100,19 @@ spa.fake = (function () {
             listchange_idto = undefined;
           }
           send_listchange();
+        }
+
+        // サーバへの「updateavatar」メッセージとデータの送信をシミュレートする
+        if ( msg_type === 'updateavatar' && callback_map.listchange ) {
+          // 「listchange」メッセージの受信をシュミレートする
+          for ( i = 0; i < peopleList.length; i++ ) {
+            if ( peopleList[ i ]._id === data.person_id ) {
+              peopleList[ i ].css_map = data.css_map;
+              break;
+            }
+          }
+          // 「listchange」メッセージ用のコールバックを実行する
+          callback_map.listchange([ peopleList ]);
         }
       };
 
--- js/spa.model.js
+++ js/spa.model.js
@@ -161,7 +161,8 @@ spa.model = (function (){
             _publish_listchange, _publish_updatechat,
             _update_list, _leave_chat, join_chat,
 
-            get_chatee, join_chat, send_msg, set_chatee,
+            get_chatee, join_chat, send_msg,
+            set_chatee, update_avatar,
             chatee = null;
 
         // 内部メソッド開始
@@ -302,12 +303,26 @@ spa.model = (function (){
             return true;
         };
 
+        // avatar_update_mapは以下の形式を持つべき。
+        // { person_id : <string>, css_map : {
+        //     top : <int>, left : <int>,
+        //     'background-color' : <string>
+        // }};
+
+        update_avatar = function ( avatar_update_map ) {
+            var sio = isFakeData ? spa.fake.mockSio : spa.data.getSio();
+            if ( sio ) {
+                sio.emit( 'updateavatar', avatar_update_map );
+            }
+        };
+
         return {
-            _leave    : _leave_chat,
-            get_chatee: get_chatee,
-            join      : join_chat,
-            send_msg  : send_msg,
-            set_chatee: set_chatee
+            _leave       : _leave_chat,
+            get_chatee   : get_chatee,
+            join         : join_chat,
+            send_msg     : send_msg,
+            set_chatee   : set_chatee,
+            update_avatar: update_avatar
         };
     }());
 
